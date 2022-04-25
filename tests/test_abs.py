9ae05fba8679afefd2681bed4c217cdb73b5096b
Khalid Hakizimana <hakizimanakp@gmail.com>
2022-04-25 16:01:48 +0800

    Initial Push

---
+++ tests/test_abs.py
@@ -0,0 +1,4 @@
+from unofficial_toolbox.absolute import absolute_value
+
+def test_func():
+    assert absolute_value(-2) > 0
---
+++ tests/test_team.py
@@ -0,0 +1,4 @@
+from unofficial_toolbox.team import the_team
+
+def test_team():
+    assert the_team(["Larisa", "Khalid", "Rachel"]) == None
